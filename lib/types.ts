1d7710e3d5206068f73eed4051dfbb1c3bbf4541
Chust3r <kriptoortega@gmail.com>
2024-08-21 17:54:00 -0600

    Add types to drizzle

--- lib/types.ts
+++ lib/types.ts
@@ -3,6 +3,8 @@ import { schema } from '~drizzle/schema'
 
 export type Room = InferSelectModel<typeof schema.rooms>
 
+export type RoomWithMessages = Room & { messages: number }
+
 export type ServerActionResponse = {
 	status: 'success' | 'error'
 	message?: string
