ccdb073b88af40bf5f4fb056dcbc169250a782c1
Chust3r <kriptoortega@gmail.com>
2024-08-21 18:53:30 -0600

    File structure to room

--- app/(dashboard)/(room)/dashboard/room/[slug]/events/page.tsx
+++ app/(dashboard)/(room)/dashboard/room/[slug]/events/page.tsx
@@ -1,7 +1,11 @@
-import { Profile } from '~components/account/profile'
-
 function Page() {
-	return <Profile />
+	return (
+		<section className='w-full h-full grid place-content-center'>
+			<p className='text-7xl font-extrabold text-accent-foreground/10'>
+				EVENTS
+			</p>
+		</section>
+	)
 }
 
 export default Page
--- app/(dashboard)/(room)/dashboard/room/[slug]/webhooks/page.tsx
+++ app/(dashboard)/(room)/dashboard/room/[slug]/webhooks/page.tsx
@@ -2,7 +2,7 @@ function Page() {
 	return (
 		<section className='w-full h-full grid place-content-center'>
 			<p className='text-7xl font-extrabold text-accent-foreground/10'>
-				QUICKSTART
+				WEBHOOKS
 			</p>
 		</section>
 	)
