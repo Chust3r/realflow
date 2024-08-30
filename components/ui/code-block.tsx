577f880f935eec6eadf04326a29182776f9b2a22
Chust3r <kriptoortega@gmail.com>
2024-08-30 14:59:48 -0600

    Add custom classes

--- components/ui/code-block.tsx
+++ components/ui/code-block.tsx
@@ -1,17 +1,19 @@
 import { CopyToClipboard } from '~ui/copy-to-clipboard'
-import { Alert, AlertDescription,} from '~ui/alert'
+import { Alert, AlertDescription } from '~ui/alert'
 import { highlightCode, extractCodeBlock } from '~lib/highlight-code'
+import { cn } from '~lib/utils'
 
 interface Props {
 	code: string
+	className?: string
 }
 
-export async function CodeBlock({ code }: Props) {
+export async function CodeBlock({ code,className }: Props) {
 	const highlightedCode = await highlightCode(code)
 	const codeToCopy = extractCodeBlock(code)
 
 	return (
-		<Alert className='col-span-12 bg-accent/10 relative'>
+		<Alert className={cn('bg-accent/10 relative', className)}>
 			<CopyToClipboard
 				data={codeToCopy}
 				className='absolute top-0 right-0 m-1.5'
