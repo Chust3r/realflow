758bfb175b0841799f181c86d17e7cc1f2a83c96
Chust3r <kriptoortega@gmail.com>
2024-08-30 14:48:18 -0600

    Update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,18 @@
-	/** @type {import('next').NextConfig} */
-	const nextConfig = {
-		async redirects() {
-			return [
-				{
-					source: '/dashboard/channels',
-					destination: '/dashboard/rooms',
-					permanent: false,
-				},
-				{
-					source: '/dashboard',
-					destination: '/dashboard/rooms',
-					permanent: false,
-				},
-				{
-					source: '/dashboard/room/:slug',
-					destination: '/dashboard/room/:slug/overview',
-					permanent: false,
-				},
-			]
-		},
-	experimental: {
-		turbo: {
-			rules: {
-				'*.svg': {
-					loaders: ['@svgr/webpack'],
-					as: '*.js',
-				},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: '/dashboard',
+				destination: '/dashboard/rooms',
+				permanent: false,
 			},
-		},
+			{
+				source: '/dashboard/room/:slug',
+				destination: '/dashboard/room/:slug/overview',
+				permanent: false,
+			},
+		]
 	},
 }
 
