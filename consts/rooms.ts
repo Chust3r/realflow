500d9cf37a7bbe4ff45930509a0193bbb2df52f0
Chust3r <kriptoortega@gmail.com>
2024-09-02 15:06:08 -0600

    Set MAX_SECRET_KEYS

--- consts/rooms.ts
+++ consts/rooms.ts
@@ -1,3 +1,5 @@
 export const MAX_ROOMS = +process.env.MAX_ROOMS! || 3
 
 export const MAX_CONNECTIONS = +process.env.MAX_CONNECTIONS! || 20
+
+export const MAX_SECRET_KEYS = +process.env.MAX_SECRET_KEYS! || 3
