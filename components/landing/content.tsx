1f62c667ba14d6513f1d23b1c94c313e924e7012
Chust3r <kriptoortega@gmail.com>
2024-09-11 16:14:17 -0600

    Update dependencies

--- components/landing/content.tsx
+++ components/landing/content.tsx
@@ -7,18 +7,18 @@ export function Content() {
 		<section className='w-full h-full mx-auto max-w-5xl flex flex-col items-center justify-center gap-8 flex-grow py-2'>
 			<div className='max-w-4xl flex flex-col gap-3 items-center'>
 				<h1 className='text-5xl tracking-wide font-medium text-center text-balance'>
-					Effortless WebSocket Management
+					Seamless WebSocket Management
 				</h1>
 				<h2 className='text-muted-foreground text-5xl tracking-wide font-medium text-center text-balance'>
-					Powered by Socket.IO
+					Built into RealTime
 				</h2>
 			</div>
 			<div className='max-w-3xl flex flex-col gap-5 items-center'>
 				<p className='text-muted-foreground text-center text-sm md:text-base text-pretty'>
-					Realflow is an open-source project that simplifies WebSocket
-					management. Powered by Socket.IO, it provides developers with an
-					easy way to create, monitor, and manage WebSocket connections for
-					real-time communication in web applications.
+					Realflow is an open-source project that streamlines WebSocket
+					management. Specifically designed for the platform, it enables
+					developers to easily create, monitor, and manage WebSocket
+					connections for real-time communication in web applications
 				</p>
 				<div className='flex gap-3'>
 					<Button asChild>
