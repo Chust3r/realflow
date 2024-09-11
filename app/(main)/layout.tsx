e2c0f0a41496e6b90ca51c26e500e9e6ad98d592
Chust3r <kriptoortega@gmail.com>
2024-09-11 16:36:24 -0600

    Add magic ui

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '~ui/grid'
+import { GridPattern } from '~components/magicui/animated-grid-pattern'
 import type { Metadata } from 'next'
 
 interface Props {
@@ -9,12 +9,14 @@ function Layout({ children }: Props) {
 	return (
 		<div className='w-full h-fit min-h-dvh grid place-items-center overflow-hidden relative'>
 			{children}
-			<Grid
-				strokeDasharray={'4 2'}
-				width={80}
-				height={80}
-				className='[mask-image:radial-gradient(400px_circle_at_center,white,transparent)] inset-x-0 inset-y-[-30%] h-[200%] skew-y-12'
-			/>
+			<GridPattern
+				numSquares={30}
+				maxOpacity={0.1}
+				duration={3}
+				repeatDelay={1}
+				width={60}
+				height={60}
+				className='[mask-image:radial-gradient(500px_circle_at_center,white,transparent)] inset-x-0 inset-y-[-30%] h-[200%] skew-y-12'/>
 			<span className='absolute inset-0 w-full h-5 rounded-full bg-primary opacity-30 blur-3xl -top-5'></span>
 			<span className='absolute w-full h-5 rounded-full bg-primary opacity-20 blur-3xl -bottom-5'></span>
 		</div>
