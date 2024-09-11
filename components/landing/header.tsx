5206f8485c941415fe18bd8ae4d05d4fd7953f1d
Chust3r <kriptoortega@gmail.com>
2024-09-11 17:11:42 -0600

    Add padding main content

--- components/landing/header.tsx
+++ components/landing/header.tsx
@@ -6,7 +6,7 @@ import { GithubStars } from './githubStars'
 export function Header() {
 	return (
 		<header className='border-b py-3'>
-			<div className='max-w-5xl mx-auto flex justify-between'>
+			<div className='max-w-5xl mx-auto flex justify-between px-3 md:px-0'>
 				<div className='flex gap-2 items-center'>
 					<RealFlow className='size-8 stroke-foreground fill-foreground' />
 					<h4 className='text-xl truncate'>RealFlow</h4>
